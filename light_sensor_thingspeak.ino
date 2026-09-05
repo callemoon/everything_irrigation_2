241d39f7108945c29099dc945c74f8ea96369823
callemoon <calle.moon@gmail.com>
2026-09-05 14:11:44 +0200

    updated by sending battery voltage in field2

--- light_sensor_thingspeak.ino
+++ light_sensor_thingspeak.ino
@@ -2,6 +2,7 @@
 #include <HTTPClient.h>
 #include <Wire.h>
 #include <BH1750.h>
+#include "esp_sleep.h"
 
 // ==============================
 // WiFi
@@ -16,6 +17,20 @@ const char* thingspeakURL =
   "https://api.thingspeak.com/update?api_key=";
 
 // ==============================
+// GPIO
+// ==============================
+#define BATTERY_PIN 1
+#define LED_PIN 0
+
+// ==============================
+// Batterispänningsdelare
+// 47k från batteri till GPIO1
+// 10k från GPIO1 till GND
+// ==============================
+const float R1 = 47000.0;
+const float R2 = 10000.0;
+
+// ==============================
 // I2C
 // ==============================
 #define SDA_PIN 20
@@ -23,18 +38,79 @@ const char* thingspeakURL =
 
 BH1750 lightMeter;
 
-// Skicka data var 20:e sekund
-const unsigned long interval = 20000;
-unsigned long previousMillis = 0;
+// ==============================
+// Deep sleep
+// ==============================
+const uint64_t SLEEP_TIME_US = 30ULL * 1000000ULL;
+
+
+// ==========================================
+// Läs batterispänning
+// ==========================================
+float readBatteryVoltage()
+{
+  // LED PÅ under mätningen
+  digitalWrite(LED_PIN, HIGH);
+
+  delay(5);
+
+  // Läs ADC flera gånger
+  const int samples = 10;
+  uint32_t sum = 0;
+
+  for (int i = 0; i < samples; i++)
+  {
+    sum += analogRead(BATTERY_PIN);
+    delay(2);
+  }
+
+  float adcValue = sum / (float)samples;
+
+  // Omvandla ADC-värdet till spänning
+  float adcVoltage = (adcValue / 4095.0) * 3.3;
+
+  // Räkna tillbaka batterispänningen
+  float batteryVoltage =
+    adcVoltage * ((R1 + R2) / R2);
+
+  // LED AV direkt efter mätningen
+  digitalWrite(LED_PIN, LOW);
+
+  return batteryVoltage;
+}
+
+
+// ==========================================
+// Deep sleep
+// ==========================================
+void goToDeepSleep()
+{
+  Serial.println("Går till deep sleep...");
+  Serial.flush();
+
+  esp_sleep_enable_timer_wakeup(SLEEP_TIME_US);
+
+  esp_deep_sleep_start();
+}
 
 
 void setup()
 {
   Serial.begin(115200);
-  delay(1000);
+  delay(500);
 
   Serial.println();
-  Serial.println("ESP32-C3 + BH1750 + ThingSpeak");
+  Serial.println("ESP32-C3 + BH1750 + Batteri + ThingSpeak");
+
+  // ============================
+  // GPIO
+  // ============================
+  pinMode(BATTERY_PIN, INPUT);
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  analogReadResolution(12);
 
   // ============================
   // Starta I2C
@@ -51,11 +127,30 @@ void setup()
   else
   {
     Serial.println("FEL: BH1750 hittades inte!");
+
     while (1)
     {
       delay(1000);
     }
   }
+
+  // ============================
+  // Läs batterispänning
+  // ============================
+  float batteryVoltage = readBatteryVoltage();
+
+  Serial.print("Batteri: ");
+  Serial.print(batteryVoltage, 2);
+  Serial.println(" V");
+
+  // ============================
+  // Läs ljus
+  // ============================
+  float lux = lightMeter.readLightLevel();
+
+  Serial.print("Ljus: ");
+  Serial.print(lux);
+  Serial.println(" lux");
 
   // ============================
   // Anslut till WiFi
@@ -64,78 +159,76 @@ void setup()
 
   Serial.print("Ansluter till WiFi");
 
-  while (WiFi.status() != WL_CONNECTED)
+  unsigned long startTime = millis();
+
+  while (WiFi.status() != WL_CONNECTED &&
+         millis() - startTime < 10000)
   {
-    delay(500);
+    delay(250);
     Serial.print(".");
   }
 
   Serial.println();
-  Serial.println("WiFi ansluten!");
-  Serial.print("IP-adress: ");
-  Serial.println(WiFi.localIP());
+
+  // ============================
+  // Skicka till ThingSpeak
+  // ============================
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    Serial.println("WiFi ansluten!");
+
+    HTTPClient http;
+
+    String url = String(thingspeakURL) +
+                 "&field1=" + String(lux, 2) +
+                 "&field2=" + String(batteryVoltage, 2);
+
+    Serial.print("Skickar till ThingSpeak: ");
+    Serial.println(url);
+
+    http.begin(url);
+
+    int httpResponseCode = http.GET();
+
+    if (httpResponseCode > 0)
+    {
+      Serial.print("HTTP svar: ");
+      Serial.println(httpResponseCode);
+
+      String response = http.getString();
+
+      Serial.print("ThingSpeak svar: ");
+      Serial.println(response);
+    }
+    else
+    {
+      Serial.print("HTTP-fel: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+  }
+  else
+  {
+    Serial.println("WiFi kunde inte anslutas!");
+  }
+
+  // ============================
+  // Stäng av WiFi
+  // ============================
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+
+  // ============================
+  // Sov i 30 sekunder
+  // ============================
+  goToDeepSleep();
 }
 
 
 void loop()
 {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval)
-  {
-    previousMillis = currentMillis;
-
-    // ==========================
-    // Läs ljussensor
-    // ==========================
-    float lux = lightMeter.readLightLevel();
-
-    Serial.print("Ljus: ");
-    Serial.print(lux);
-    Serial.println(" lux");
-
-
-    // ==========================
-    // Kontrollera WiFi
-    // ==========================
-    if (WiFi.status() == WL_CONNECTED)
-    {
-      HTTPClient http;
-
-      // Bygg URL
-      String url = String(thingspeakURL) + String(lux, 2);
-
-      Serial.print("Skickar till ThingSpeak: ");
-      Serial.println(url);
-
-      // Starta HTTP
-      http.begin(url);
-
-      // GET
-      int httpResponseCode = http.GET();
-
-      // Kontrollera svar
-      if (httpResponseCode > 0)
-      {
-        Serial.print("HTTP svar: ");
-        Serial.println(httpResponseCode);
-
-        String response = http.getString();
-
-        Serial.print("ThingSpeak svar: ");
-        Serial.println(response);
-      }
-      else
-      {
-        Serial.print("HTTP-fel: ");
-        Serial.println(httpResponseCode);
-      }
-
-      http.end();
-    }
-    else
-    {
-      Serial.println("WiFi inte anslutet!");
-    }
-  }
+  // Används inte.
+  // ESP32 startar om från setup()
+  // efter deep sleep.
 }
